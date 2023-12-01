980347605db45d29c6d58b09cc835c62bf44c8ef
Vadim Vasiatullin <vasiatyllin@yandex.ru>
2023-12-01 15:01:17 +0300

    Поправил дизайн

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -9,14 +9,23 @@
 import UIKit
 
 class WelcomeViewController: UIViewController {
-
+    
     @IBOutlet weak var titleLabel: UILabel!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-       
+        titleLabel.text = ""
+        var charIdex = 0.0
+        let titleText = "⚔️ ValhallaChat ⚔️"
+        for leter in titleText {
+            Timer.scheduledTimer(withTimeInterval: 0.1 * Double(charIdex) , repeats: false) { (timer) in
+                
+                self.titleLabel.text?.append(leter)
+            }
+            charIdex += 1
+        }
+        
+        
     }
-    
-
 }
