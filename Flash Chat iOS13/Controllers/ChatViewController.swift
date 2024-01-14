2974fa185328ddd6cdb4c30ae17158d5dcc2a87a
Vadim Vasiatullin <vasiatyllin@yandex.ru>
2024-01-14 18:36:55 +0300

    добавил сортировку по отправленным

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -32,7 +32,7 @@ class ChatViewController: UIViewController {
     func loadMessages() {
        
         
-        db.collection(K.FStore.collectionName).addSnapshotListener { (querySnapshot, error) in
+        db.collection(K.FStore.collectionName).order(by: K.FStore.dateField).addSnapshotListener { (querySnapshot, error) in
             
             self.messages = []
             if let e = error {
@@ -58,7 +58,7 @@ class ChatViewController: UIViewController {
     
     @IBAction func sendPressed(_ sender: UIButton) {
         if let messageBody = messageTextfield.text, let messageSender = Auth.auth().currentUser?.email {
-            db.collection(K.FStore.collectionName).addDocument(data: [K.FStore.senderField: messageSender, K.FStore.bodyField: messageBody]) { (error) in
+            db.collection(K.FStore.collectionName).addDocument(data: [K.FStore.senderField: messageSender, K.FStore.bodyField: messageBody, K.FStore.dateField: Date().timeIntervalSince1970]) { (error) in
                 if let e = error {
                     print("Была проблема с сохранением данных в Firestore")
                 } else {
